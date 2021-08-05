5fcdcac0b8e0a54aa53203332671380b7f0ef917
hyunlee214 <hyun110214@naver.com>
2021-08-05 23:51:30 +0900

    log관리파일생성

--- app/app.js
+++ app/app.js
@@ -3,11 +3,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
-dotenv.config();
+const morgan = require('morgan');
+const fs = require('fs');
 
 const app = express();
+dotenv.config();
 
 const home = require('./src/routes/home');
+
+const accessLogSystem = fs.createWriteStream(
+  `${__dirname}/log/access.log`,
+  { flags: "a" }
+);
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
@@ -15,6 +22,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(morgan("dev", { stream: accessLogSystem }));
+// app.use(morgan('combined'));
 
 app.use('/', home);
 
