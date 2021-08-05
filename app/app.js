d024b0d9b6b488dc917957cff35812e7bf6d5a4e
hyunlee214 <hyun110214@naver.com>
2021-08-06 01:46:26 +0900

    log모듈분류

--- app/app.js
+++ app/app.js
@@ -4,17 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const fs = require('fs');
 
 const app = express();
 dotenv.config();
 
+const accessLogStream = require('./src/config/log');
+
+
 const home = require('./src/routes/home');
 
-const accessLogSystem = fs.createWriteStream(
-  `${__dirname}/log/access.log`,
-  { flags: "a" }
-);
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
@@ -22,7 +20,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(morgan("dev", { stream: accessLogSystem }));
+app.unsubscribe(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 // app.use(morgan('combined'));
 
 app.use('/', home);
