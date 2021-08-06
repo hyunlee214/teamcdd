bd2da337379367462c6406682cb766e31cf6b418
hyunlee214 <hyun110214@naver.com>
2021-08-06 15:40:46 +0900

    출석부1차 구현완

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,6 +7,11 @@ const id = document.querySelector('#id'),
   loginBtn.addEventListener('click',login);
 
   function login() {
+    if (!id.value)
+    return alert('아이디는 입력하셔야죠!!!');
+    if (!psword.value)
+    return alert('비밀번호는 입력하셔야죠!!!');
+
     const req = {
       id : id.value,
       psword: psword.value,
