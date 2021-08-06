06482a39080c510a4c4365fec6b867dd05db88f9
hyunlee214 <hyun110214@naver.com>
2021-08-06 13:43:54 +0900

    콘솔/파일log동시출력

--- app/src/config/logger.js
+++ app/src/config/logger.js
@@ -1,28 +1,45 @@
-const { createLogger, transports, format } = require('winston');
+const { createLogger, transports, format, transport } = require('winston');
 const { combine, timestamp, printf, label, json, simple, colorize } = format;
 
+const printFormat = printf(({ timestamp, label, level, message }) => {
+  return `${timestamp} [${label}] ${level} ${message}`;
+});
 
 const printLogFormat = combine(
   label({
     label: "테스트 레이블",
   }),
-  colorize(),
+  // colorize(),    // 파일에 로그추가할때는 없는게나음
   timestamp({
     format: "YYYY-MM-DD HH:mm:dd"
   }),
-  printf(({ timestamp, label, level, message }) => {
-    return `${timestamp} [${label}] ${level} ${message}`;
-  })
+  printFormat
 );
 
  
 const logger = createLogger({
       transports: [ 
+        new transports.File({
+          filename: "access.log",
+          dirname: "./logs",
+          level: "info",
+          format: printLogFormat,
+        }),
         new transports.Console({
           level: "info",
           format: printLogFormat,
         }),
       ],
     });
+
+    if (process.env.NODE_ENV !== "production") {
+      logger.add(
+        new transports.Console({
+          level: "info",
+          format: printLogFormat,
+        })
+      );
+    }
+
 
 module.exports = logger;
