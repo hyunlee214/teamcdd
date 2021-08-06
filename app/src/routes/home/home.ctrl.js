9ee5f674b57d1f8b08f7bb49213683a2a6f3703c
hyunlee214 <hyun110214@naver.com>
2021-08-07 03:15:30 +0900

    log함수로최적화+id변수비어있음

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -21,33 +21,54 @@ const output = {
 const process = {
   login: async (req, res) => {
     const user = new User(req.body);
+
     const response = await user.login();
-    if (response.err)
-      logger.error(
-        `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
-      );
-    else
-      logger.info (
-      `POST /login 200 Response: "로그인한 사람: ${req.body.id}, success: ${response.success}, msg: ${response.msg}"`
-    );
+
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: "",
+    };
+    
+    const id = {
+
+    }
+
+    log(response, url, id);
     return res.json(response);
   },
+
   register: async (req, res) => {
     const user = new User(req.body);
     const response = await user.register();
-    if (response.err)
-      logger.error(
-        `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
-      );
-    else
-      logger.info (
-        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      );
+
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: "",
+    };
+
+    log(response, url, id);
     return res.json(response);
-  },
+
+    log(response); 
+    return res.json(response);
+  },   
 };
 
 module.exports = {
   output,
   process
 };
+
+const log = (response, url, id) => {
+  if (response.err) {
+    logger.error(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`
+     ); 
+  } else {
+      logger.info (
+        `${url.method} ${url.path} ${url.status} Response: "누가출석했죠?:${response.id},  ${response.success},  ${response.msg}"`
+      );
+    }
+  }
