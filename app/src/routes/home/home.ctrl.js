8e7e9d7a07a962ce0aa08a3472204a21546716fe
hyunlee214 <hyun110214@naver.com>
2021-08-10 22:26:50 +0900

    로그파일,ctrl파일 수정

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -31,7 +31,7 @@ const process = {
     };
     
     const id = {
-
+      status: response.id
     }
 
     log(response, url, id);
@@ -45,7 +45,7 @@ const process = {
     const url = {
       method: "POST",
       path: "/register",
-      status: response.err ? 400 : 200,
+      status: response.err ? 400 : 201,   // 가입은 데이터 생성 => 201
     };
 
     log(response, url, id);
@@ -61,7 +61,7 @@ module.exports = {
   process
 };
 
-const log = (response, url, id) => {
+const log = (response, url, id1) => {
   if (response.err) {
     logger.error(
       `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`
@@ -72,3 +72,62 @@ const log = (response, url, id) => {
       );
     }
   }
+
+  
+
+
+
+/*  
+  "use strict";
+
+const logger = require('../../config/logger');
+const User = require('../../models/User');
+
+const output = {
+  home : (req, res) => {
+    logger.info(`GET/200 "홈 화면으로 이동"`);
+    res.render('home/index');
+  },
+  login: (req, res) => {
+    logger.info(`GET/login 200 "로그인 화면으로 이동"`);
+    res.render('home/login');
+  },
+  register: (req, res) => {
+    logger.info(`GET/register 200 "회원가입 화면으로 이동"`);
+    res.render('home/register');
+  },
+};
+const process = {
+  login: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.login();
+    if (response.err)
+      logger.error(
+        `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
+      );
+    else
+      logger.info (
+      `POST /login 200 Response: "누가출석했죠?: ${req.body.id}, success: ${response.success}, msg: ${response.msg}"`
+    );
+    return res.json(response);
+  },
+  register: async (req, res) => {
+    const user = new User(req.body);
+    const response = await user.register();
+    if (response.err)
+      logger.error(
+        `POST /login 200 Response: "success: ${response.success}, ${response.err}"`
+      );
+    else
+      logger.info (
+        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
+      );
+    return res.json(response);
+  },
+};
+module.exports = {
+  output,
+  process
+};
+
+*/
