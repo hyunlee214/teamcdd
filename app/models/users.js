3c1375b26199935613513ffda07a870a9dba1ac3
hyunlee214 <hyun110214@naver.com>
2021-09-02 20:34:25 +0900

    모델 재정의

--- app/models/users.js
+++ app/models/users.js
@@ -3,10 +3,6 @@
 module.exports = (sequelize, DataTypes) => {
 
   const users = sequelize.define('users', {
-    // id : {
-    //   type: DataTypes.STRING(30),
-    //   allowNull: false
-    // },
     name : {
       type: DataTypes.STRING(30),
       allowNull: false
@@ -15,16 +11,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING(30),
       allowNull: false
     }
-  }, 
-  {
-    charset: 'utf8',
-    collate: "utf8_general_ci",
-    tableName: "cddtable",
-    timestamps: true,
-    paranoid: true
-  })
-  // users.associate = (models) => {
-  //     // users.belongsTo(models.LogType, { foreginKey: 'usersId' })
-  //   };
-    return users
+  });
+    return users;
   }
